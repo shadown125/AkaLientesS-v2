06fc44cf1e62b32f0c2126afde72f4700871d5e3
YoLoN <shadyolown13@onet.pl>
2022-05-28 14:26:47 +0200

    :sparkles: Add meta tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,12 +6,27 @@ export default {
         },
         meta: [
             { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: '' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
+            { 'http-equiv': 'X-UA-Compatible', content: 'ie=edge' },
+            { name: 'author', content: 'Dawid Oleksiuk' },
+            { name: 'description', content: 'Discover my Portfolio to get information about me and also inspiration.' },
+            { hid: 'description', name: 'description', content: 'Discover my Portfolio to get information about me and also inspiration.' },
+            { property: 'og:title', content: 'AkaLientesS' },
+            { property: 'og:type', content: 'website' },
+            { property: 'og:description', content: 'Discover my Portfolio to get information about me and also inspiration.' },
+            { property: 'og:local', content: 'pl_PL' },
+            { property: 'og:image', content: '/seoAkalientess.png' },
+            { property: 'og:url', content: 'https://www.akalientess.com/' },
+            { property: 'og:locale:alternate', content: 'en_US' },
+            { name: 'twitter:site', content: '@DawidOleksiuk' },
+            { name: 'twitter:title', content: 'AkaLientesS' },
+            { name: 'twitter:description', content: 'Discover my Portfolio to get information about me and also inspiration.' },
+            { name: 'twitter:image', content: '/seoAkalientess.png' },
             { name: 'format-detection', content: 'telephone=no' }
         ],
         link: [
-            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+            { rel: 'icon', type: 'image/png', href: '/favicon.png' },
+            { rel: 'apple-touch-icon', href: '/favicon.png' }
         ]
     },
     components: true,
