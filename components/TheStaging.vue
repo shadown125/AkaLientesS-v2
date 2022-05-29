69b5d1206d1ee81e7c0144f4fb228a3899f5d3df
YoLoN <shadyolown13@onet.pl>
2022-05-29 23:37:54 +0200

    :sparkles: Add prototype staging
    This is a prototype version of staging. The final look might completely change

---
+++ components/TheStaging.vue
@@ -0,0 +1,58 @@
+<template>
+    <section :class="['staging', active]" v-observe-visibility="isVisible">
+        <div class="wrapper">
+            <div class="grid">
+                <ul :class="['social-links', active]">
+                    <li v-for="link in links" :key="link.name">
+                        <a :href="link.link" rel="external noopener" target="_blank" :class="['icon', link.name]">
+                            <span>{{ link.name }}</span>
+                        </a>
+                    </li>
+                </ul>
+                <div class="content">
+                    <h1 class="headline h1">AkaLientesS</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit magnam obcaecati quam, quod suscipit temporibus velit vero.
+                    </p>
+                </div>
+                <div class="letters">
+                    <span>Y</span>
+                    <span>O</span>
+                    <span>L</span>
+                    <span>O</span>
+                    <span>N</span>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'TheStaging',
+    data (): {active: string, links: {link: string, name: string}[]} {
+        return {
+            active: '',
+            links: [{
+                link: 'https://github.com/shadown125',
+                name: 'github'
+            },
+            {
+                link: 'https://twitter.com/DawidOleksiuk',
+                name: 'twitter'
+            },
+            {
+                link: 'https://www.linkedin.com/in/dawid-ol-2478311a4/',
+                name: 'linkedin'
+            }]
+        }
+    },
+    methods: {
+        isVisible (visible: boolean) {
+            if (visible) {
+                this.active = 'is-active'
+            }
+        }
+    }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,8 @@
             :particlesInit="particlesInit"
         />
         <TheNavigation />
+        <TheStaging />
+        <TheFloatingLines />
     </div>
 </template>
 
