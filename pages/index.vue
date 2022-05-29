a17681ee9a9d9878b34579a1242b0f2dd8041668
YoLoN <shadyolown13@onet.pl>
2022-05-29 23:35:15 +0200

    :sparkles: Initialise and config particle js

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,10 @@
 <template>
     <div class="app">
+        <Particles
+            id="tsparticles"
+            :options="options"
+            :particlesInit="particlesInit"
+        />
         <TheNavigation />
     </div>
 </template>
@@ -8,10 +13,62 @@
 import Vue from 'vue'
 import '../static/main.css'
 import VueObserveVisibility from 'vue-observe-visibility'
+import Particles from 'particles.vue'
+import type { Engine } from 'tsparticles-engine'
+import { loadFull } from 'tsparticles'
 
 Vue.use(VueObserveVisibility)
+Vue.use(Particles)
 
 export default Vue.extend({
-    name: 'IndexPage'
+    name: 'IndexPage',
+    data () {
+        return {
+            options: {
+                interactivity: {
+                    events: {
+                        onHover: {
+                            mode: 'repulse',
+                            enable: true
+                        }
+                    }
+                },
+                modes: {
+                    push: {
+                        quantity: 15
+                    },
+                    repulse: {
+                        distance: 30
+                    }
+                },
+                particles: {
+                    links: {
+                        distance: 40,
+                        enable: true
+                    },
+                    move: {
+                        enable: true
+                    },
+                    opacity: {
+                        value: {
+                            min: 0.1,
+                            max: 0.4
+                        }
+                    },
+                    size: {
+                        value: {
+                            min: 1,
+                            max: 3
+                        }
+                    }
+                }
+            }
+        }
+    },
+    methods: {
+        particlesInit (engine: Engine): Promise<void> {
+            loadFull(engine)
+        }
+    }
 })
 </script>
