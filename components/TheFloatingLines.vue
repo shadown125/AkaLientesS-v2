e61705091f1027a8be31dec531522897fcc14289
YoLoN <shadyolown13@onet.pl>
2022-05-29 23:31:53 +0200

    :sparkles: Add floating lines feature

---
+++ components/TheFloatingLines.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="section floating-lines">
+        <div />
+        <div />
+        <div />
+        <div />
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'TheFloatingLines'
+}
+</script>
